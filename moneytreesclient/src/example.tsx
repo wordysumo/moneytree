b37bcda814764f9309d9037816615919bdcb713d
DevMjee <xzpx48@durham.ac.uk>
2022-11-19 17:40:55 +0000

    inital button design

--- moneytreesclient/src/example.tsx
+++ moneytreesclient/src/example.tsx
@@ -6,8 +6,19 @@ export const Example = () => {
   const constraintsRef = useRef(null);
 
   return (
-    <motion.div className="container" ref={constraintsRef}>
-      <motion.div className="item" drag dragConstraints={constraintsRef} />
-    </motion.div>
+    <div>
+      <div className="econtainer">
+      <motion.div className="container" ref={constraintsRef}>
+        <motion.div className="item" drag dragConstraints={constraintsRef} />
+      </motion.div>
+      </div>
+      
+      <div className="buttons">        
+        <button className="water">Water</button>                
+        <button className="feed">Feed</button>                
+        <button className="catch">Catch</button>      
+        <button className="donate">Donate</button>   
+      </div>
+    </div>
   );
 };
